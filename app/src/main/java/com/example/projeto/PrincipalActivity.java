2b5d31bb7d2971f002c7716fc7965d63fef4aebb
Mateus-pc <burklemateus@gmail.com>
2025-10-02 10:38:17 -0300

    att tela principal para ir para tela de Tarefas

--- app/src/main/java/com/example/projeto/PrincipalActivity.java
+++ app/src/main/java/com/example/projeto/PrincipalActivity.java
@@ -2,10 +2,20 @@ package com.example.projeto;
 
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Button;
+import android.content.Intent;
+
 public class PrincipalActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.principal);
+
+        Button btnAdicionarTarefa = findViewById(R.id.btnAdicionarTarefa);
+
+        btnAdicionarTarefa.setOnClickListener(v ->{
+            Intent intent = new Intent(PrincipalActivity.this, TarefasActivity.class);
+            startActivity(intent);
+        });
     }
 }
