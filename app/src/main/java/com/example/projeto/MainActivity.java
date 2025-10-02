6c08224f7c26ffb6617cf9da7f7da5b2610f53bd
Mateus-pc <burklemateus@gmail.com>
2025-10-02 10:25:25 -0300

    att id nas telas e troca de tela de cadastro pra tela principal

--- app/src/main/java/com/example/projeto/MainActivity.java
+++ app/src/main/java/com/example/projeto/MainActivity.java
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.principal);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.tela1), (v, insets) -> {
+        setContentView(R.layout.cadastro);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.telaCadastro), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button btnCadastrar = findViewById(R.id.btnCadastrar);
+        btnCadastrar.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, PrincipalActivity.class);
+            startActivity(intent);
         });
     }
 }
