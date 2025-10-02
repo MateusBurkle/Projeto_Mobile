627e063c435273a033032c2a604381a163c18d33
Mateus-pc <burklemateus@gmail.com>
2025-10-02 10:25:31 -0300

    att id nas telas e troca de tela de cadastro pra tela principal

---
+++ app/src/main/java/com/example/projeto/PrincipalActivity.java
@@ -0,0 +1,11 @@
+package com.example.projeto;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class PrincipalActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.principal);
+    }
+}
